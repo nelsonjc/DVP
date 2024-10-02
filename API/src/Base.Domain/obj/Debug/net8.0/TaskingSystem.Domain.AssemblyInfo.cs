bfe44c07a1b1ab23d42ce8e56d225bde59a0f8e8
Nelson Fabian Jaramillo Causil <nelsonjaramilloc@gmail.com>
2024-10-01 22:19:46 -0500

    Eliminar archivos de Visual Studio y actualizar .gitignore

--- API/src/Base.Domain/obj/Debug/net8.0/TaskingSystem.Domain.AssemblyInfo.cs
+++ API/src/Base.Domain/obj/Debug/net8.0/TaskingSystem.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskingSystem.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e448b8a78887e4ef4b135716d209aa92697578ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65f5ee2d654a364c6041346a17c335dfd437f764")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskingSystem.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskingSystem.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- API/tests/Base.Test/obj/Debug/net8.0/TaskingSystem.Tests.AssemblyInfo.cs
+++ API/tests/Base.Test/obj/Debug/net8.0/TaskingSystem.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskingSystem.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e448b8a78887e4ef4b135716d209aa92697578ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65f5ee2d654a364c6041346a17c335dfd437f764")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskingSystem.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskingSystem.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
